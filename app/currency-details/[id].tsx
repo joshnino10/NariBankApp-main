5af4daabc72770d41d781585a647b108a42255f1
[joshua nduka] <[ndukajoshua6@gmail.com]>
2025-12-08 09:07:17 +0100

    j

--- app/currency-details/[id].tsx
+++ app/currency-details/[id].tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text,  SafeAreaView, Platform, StatusBar  } from 'react-native'
 import React from 'react'
+import { Image, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 
 
 export default function SingleCurrency() {
   return (
     <SafeAreaView style={styles.SafeArea}>
-      <Text>[id]</Text>
+        <View style={styles.page}>
+            <View style={styles.header}>
+                <Image style={styles.icon} source={require('../../assets/images/arrowBack.png')} />
+                <Text style={styles.headerText}>Swap</Text>
+                <View style={styles.icon}/>
+
+            </View>
+
+        </View>
+     
     </SafeAreaView>
   )
 }
@@ -15,6 +24,29 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor:'white',
         paddingTop: Platform.OS === 'android'? StatusBar.currentHeight:0 
+    },
+
+    page:{
+        paddingHorizontal:16
+
+    },
+
+    icon:{
+      width:20,
+      height:40
+    },
+    
+    header:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+
+    },
+
+    headerText:{
+        fontSize:20,
+        fontFamily: 'intersemibold',
+        fontWeight:"600",
     }
 
 })
